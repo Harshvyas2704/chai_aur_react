9bc0792c1ee8a5363ae76576509590977c6d3b73
Harshvyas2704 <harshvyasdev@gmail.com>
2023-09-22 21:08:40 +0530

    other changes

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function App() {
   const [color, setColor] = useState("olive");
 
   const handleButtonClick = (newColor) => {
     console.log(newColor);
-    setColor(newColor)
-  }
+    setColor(newColor);
+  };
 
   return (
     <div
@@ -16,14 +16,46 @@ function App() {
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-02">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <ButtonCard name="Orange" color="orange" onClick={() => handleButtonClick('orange')} />
-          <ButtonCard name="Red" color="red" onClick={() => handleButtonClick('red')}/>
-          <ButtonCard name="Blue" color="blue" onClick={() => handleButtonClick('blue')}/>
-          <ButtonCard name="Green" color="green" onClick={() => handleButtonClick('green')}/>
-          <ButtonCard name="Teal" color="teal" onClick={() => handleButtonClick('teal')}/>
-          <ButtonCard name="Wheat" color="wheat" onClick={() => handleButtonClick('wheat')}/>
-          <ButtonCard name="Purple" color="purple" onClick={() => handleButtonClick('purple')}/>
-          <ButtonCard name="Black" color="black" onClick={() => handleButtonClick('black')}/>
+          <ButtonCard
+            name="Orange"
+            color="orange"
+            onClick={() => handleButtonClick("orange")}
+          />
+          <ButtonCard
+            name="Red"
+            color="red"
+            onClick={() => handleButtonClick("red")}
+          />
+          <ButtonCard
+            name="Blue"
+            color="blue"
+            onClick={() => handleButtonClick("blue")}
+          />
+          <ButtonCard
+            name="Green"
+            color="green"
+            onClick={() => handleButtonClick("green")}
+          />
+          <ButtonCard
+            name="Teal"
+            color="teal"
+            onClick={() => handleButtonClick("teal")}
+          />
+          <ButtonCard
+            name="Wheat"
+            color="wheat"
+            onClick={() => handleButtonClick("wheat")}
+          />
+          <ButtonCard
+            name="Purple"
+            color="purple"
+            onClick={() => handleButtonClick("purple")}
+          />
+          <ButtonCard
+            name="Black"
+            color="black"
+            onClick={() => handleButtonClick("black")}
+          />
         </div>
       </div>
     </div>
